90b2836cab3371e7bcd0c358888ad9fdd2d12ed3
Yongwoo Shin <amoshinihsoma@gmail.com>
2018-11-12 20:08:28 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -8,7 +8,7 @@ describe('Arrays', function() {
   describe('destructivelyAppendKitten(name)', function() {
     it('appends a kitten to the end of the kittens array', function() {
       window.kittens = ['Milo', 'Otis', 'Garfield'];
-      kittens.slice('Ralph')
+      kittens.push('Ralph')
       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield", "Ralph"])
     })
   })
